92f7874e8a1a4cd6c6edbd5249e22739dc38fbb3
Yulaco <cleochen93@gmail.com>
2023-08-16 14:11:44 -0700

    execute command

--- free_array.c
+++ free_array.c
@@ -1,12 +1,21 @@
 #include "main.h"
+
+/**
+ * free_array - Function to free memory of an array.
+ * @myarray: Pointer to array to be freed.
+ *
+ * Return: Void.
+ */
+
 void free_array(array *myarray)
 {
 	int i;
+
 	if (myarray == NULL)
 		return;
 	if (myarray->strings)
 	{
-		for (i = 0; i < myarray->count;i++)
+		for (i = 0; i < myarray->count; i++)
 		{
 			free(myarray->strings[i]);
 		}
