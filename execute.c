4c02b34e4ebe86aa2645727fd58926bc89be2a12
Yulaco <cleochen93@gmail.com>
2023-08-16 14:11:10 -0700

    now working

--- execute.c
+++ execute.c
@@ -1,11 +1,19 @@
 #include "main.h"
+
+/**
+ * execute - Function to execute a command.
+ * @myarr: Array to be executed.
+ *
+ * Return: NULL.
+ */
+
 void execute(array myarr)
 {
 	char *exe[100], cat[200] = "/bin/";
 	int j, run;
 	pid_t mypid;
 
-	if(strstr(myarr.strings[0], cat) != NULL)
+	if (strstr(myarr.strings[0], cat) != NULL)
 	{
 		exe[0] = myarr.strings[0];
 	}
@@ -14,7 +22,7 @@ void execute(array myarr)
 		strcat(cat, myarr.strings[0]);
 		exe[0] = strdup(cat);
 	}
-	for(j = 1; myarr.strings[j] != NULL; j++)
+	for (j = 1; myarr.strings[j] != NULL; j++)
 	{
 		exe[j] = myarr.strings[j];
 	}
@@ -37,5 +45,5 @@ void execute(array myarr)
 	{
 		wait(NULL);
 	}
-	
+
 }
