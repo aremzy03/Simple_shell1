1a0dfd6ddb006aa0f56a24effb09f63479cb1370
Yulaco <cleochen93@gmail.com>
2023-08-16 14:13:37 -0700

    now working

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /********STATIC LIBRARIES**********/
 #include <stdio.h>
 #include <unistd.h>
@@ -7,13 +8,27 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+
 /*********STRUCTURES*********/
-typedef struct stringArray {
+
+/**
+ * struct stringArray - Struct made up of an array of strings.
+ * @strings: First member a string.
+ * @count: Second member.
+ *
+ * Description: Struct of a combination of a character and string.
+ */
+
+typedef struct stringArray
+{
 	char **strings;
 	int count;
 } array;
+
 /***********Functions*******/
-array get_command();
+array get_command(void);
 void free_array(array *myarray);
 void execute(array myarr);
+int main(void);
+
 #endif
