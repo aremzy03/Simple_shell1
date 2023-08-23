7b30fe14dbdaf27d7d67b3d45bf88c17d6d636c9
Yulaco <cleochen93@gmail.com>
2023-08-23 01:41:53 -0700

    handled the cd command

--- directory.c
+++ directory.c
@@ -1,4 +1,12 @@
 #include "main.h"
+
+/**
+ * directory - Function changes current directory to directory
+ * specified in path.
+ * @path: Path to directory.
+ *
+ * Return: If successful 0, else -1 if error.
+ */
 int directory(char *path)
 {
 	int change;
